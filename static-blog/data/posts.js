da0e206ebcfcb307ebf0909c4ed2d1381977b3fe
Sowmya G <sowmyag.005@gmail.com>
2026-01-06 12:32:43 +0530

    Add files via upload

---
+++ static-blog/data/posts.js
@@ -0,0 +1,17 @@
+export const posts = [
+  {
+    slug: "learn-nextjs",
+    title: "Learn Next.js",
+    content: "Next.js is a powerful React framework for building web apps."
+  },
+  {
+    slug: "react-basics",
+    title: "React Basics",
+    content: "React is a JavaScript library for building user interfaces."
+  },
+  {
+    slug: "static-sites",
+    title: "Static Websites",
+    content: "Static websites are fast, secure, and easy to deploy."
+  }
+];
